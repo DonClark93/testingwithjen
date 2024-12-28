6fb9e8f1a6662fae5ef5c04b88b024c0fb02c596
Don Clark <123608546+DonClark93@users.noreply.github.com>
2024-12-28 09:58:46 -0500

    Added an example for jen

---
+++ gitStuff/gitexample.js
@@ -0,0 +1,42 @@
+/*
+This is a rundown of using git
+Git is a version control system
+you invoke the git program by using the key word "git", then telling it the command that you you it to execute
+there is usually(but not always!) a secondary or third argument that you also pass. This could be many things. you will see examples
+there are 4 main commands that you will need to use
+
+1. " git add . "
+git - invoking the program 
+add - the command that you are executing i.e. add new files, folders to the git version control system. 
+you can also add changes to files already under the git version control system
+. - this is the secondary argument, this is another way of saying "all files" otherwise you could git add each file individually
+
+2. " git commit -m "MESSAGE" "
+git - invoking the program 
+commit - you want to commit these changes to the digital record that you are controlling
+-m - a flag that states you wan to leave a message with this commit
+"MESSAGE" - whatever you want, but keep it concise
+
+3. " git push "
+git - invoking the program
+push - push a copy of the commits/records to the remote repostory for shared record keeping
+**run this at the end of your session when you want to "save" your progress to the online repo 
+
+4. " git pull "
+git - invoking the program
+push - pull a copy of the commits/records to the remote repostory for shared keeping
+**run this at the beginning of your session to pull down changes that may not have yet - wont need to likely do this often
+
+Think of it this way:
+    you PULL down changes from the remote repo
+    you ADD files to the stage
+    you COMMIT the changes to the record
+    you PUSH your record to the remote repo
+
+So add something between the curly brackets and use the git commands to add, commit, and push! 
+{
+
+
+}
+
+*/
