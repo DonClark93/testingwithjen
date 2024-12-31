1db14549035e2682bb4b8ce16ee477e2af684cba
Don Clark <123608546+DonClark93@users.noreply.github.com>
2024-12-31 00:12:03 -0500

    js stuff added

--- jsStuff/example.js
+++ jsStuff/example.js
@@ -1,3 +1,20 @@
-TBC
+//JavaScript is interpreted line by line. Each expressions can be ended with a semi colon
+let x = 5; //let - allows variable to be reassigned
+const y = 4 //const - does not allow variable to be reassigned
+// both valid expressions with or without semicolon
 
+//this allows you to print items to the command line if you are working with javascript on the server/pc
+//or in the web browser for web dev
+console.log(x)
+console.log("you can put strings here too!")
 
+//uncomment the next line and try and run the File. hint: 'node ./jsStuff/example.js'
+//y = y + x;
+//read the error, this can usually help you solve issues. comment the line again
+
+//Guess what you think the next few lines will print out
+//Uncomment the next few lines and see if you are right
+
+//let newVariable = 3;
+//let vowels = "ou";
+//console.log("I <" + newVariable + " Y" + vowels)
